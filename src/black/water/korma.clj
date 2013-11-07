ec425d3afc7d2cad53469006c5640cffa4e00cc8
Chris Allen <cma@bitemyapp.com>
2013-11-06 21:04:29 -0800

    further refactoring

--- src/black/water/korma.clj
+++ src/black/water/korma.clj
@@ -4,6 +4,7 @@
             [korma.db :as kdb]
             [korma.core :as kc]
             [clansi.core :refer :all]
+            [black.water.jdbc :refer [generic-logger]]
             [black.water.log :refer [log-sql]]
             [clj-time.core :as time]))
 
@@ -11,12 +12,7 @@
   "Hook for korma, mercifully the library has a universal, singular
    function where all queries eventually end up. <3"
   [f & args]
-  (let [start (time/now)
-        result (apply f args)
-        end (time/now)
-        time-taken (time/in-millis (time/interval start end))]
-    (log-sql (:sql-str (first args)) time-taken)
-    result))
+  (generic-logger #(:sql-str (first %)) f args))
 
 (defn decorate-korma!
   "Hooks into Korma to log SQL that gets executed."
